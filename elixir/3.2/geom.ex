91ea9243ac683d890a7323c8063f5261c5889e6a
Anders Pearson <anders@columbia.edu>
2016-03-24 21:40:11 +0000

    3.2

---
+++ elixir/3.2/geom.ex
@@ -0,0 +1,13 @@
+defmodule Geom do
+	def area(:rectangle, width, height) when width >= 0 and height >= 0 do
+		width * height
+	end
+
+  def area(:triangle, width, height) when width >= 0 and height >= 0 do
+    width * height / 2.0
+  end
+
+  def area(:ellipse, width, height) when width >= 0 and height >= 0 do
+    :math.pi() * width * height
+  end
+end
